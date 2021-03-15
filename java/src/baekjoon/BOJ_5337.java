f979732c0e3f794b261d1569db4ba51c315a6846
Changhun-Han <hch4102@naver.com>
2021-03-15 23:19:20 +0900

    feat(Changhun-Han): [BOJ, 5337] 웰컴
    https://www.acmicpc.net/problem/5337
    Signed-off-by: Changhun-Han <hch4102@naver.com>

---
+++ java/src/baekjoon/BOJ_5337.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+/**
+ * @see <a href="httpstps://www.acmicpc.net/problem/5337">
+ * https//www.acmicpc.net/problem/5337
+ * </a>
+ */
+public class BOJ_5337 {
+    
+    public static void main (String[] args) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(".  .   .\n")
+                     .append("|  | _ | _. _ ._ _  _\n")
+                     .append("|/\\|(/.|(_.(_)[ | )(/.");
+        System.out.println(stringBuilder);
+    }
+    
+}
