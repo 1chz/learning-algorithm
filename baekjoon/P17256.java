64695e496ad5308626bebb53d9efe19169395397
9m1i9n1 <9m1i9n1@gmail.com>
2021-03-17 03:34:15 +0900

    feat(Minhyun-Lee): [BOJ, 17256] 달달함이 넘쳐흘러 https://www.acmicpc.net/problem/17256

---
+++ baekjoon/P17256.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * @see <a href="https://www.acmicpc.net/problem/17256">달달함이 넘쳐흘러</a>
+ */
+public class P17256 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        System.out.println(String.format("%d %d", (b-a), b));
+    }
+}
