24b549de185a64efa79d6865dd5b6a04ac6d95af
Changhun-Han <hch4102@naver.com>
2021-03-16 22:06:34 +0900

    feat(Changhun-Han): [BOJ, 15680] 연세대학교
    https://www.acmicpc.net/problem/15680
    Signed-off-by: Changhun-Han <hch4102@naver.com>

---
+++ java/src/ChanghunHan/baekjoon/BOJ_15680.java
@@ -0,0 +1,28 @@
+package ChanghunHan.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import static java.lang.Integer.parseInt;
+
+/**
+ * @see <a href="https://www.acmicpc.net/problem/15680">
+ * https://www.acmicpc.net/problem/15680
+ * </a>
+ */
+public class BOJ_15680 {
+    
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = parseInt(br.readLine());
+        if (N == 0) {
+            System.out.print("YONSEI");
+        }
+        else {
+            System.out.print("Leading the Way to the Future");
+        }
+        br.close();
+    }
+    
+}
