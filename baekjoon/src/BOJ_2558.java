da6d91fbbd0304066043911da6e150869d2cfc45
Gyoungmin-Min <alsrudals933@naver.com>
2021-03-16 17:16:04 +0900

    feat(Gyoungmin-Min):[BOJ, 2558] A+B -2 https://www.acmicpc.net/problem/2558

---
+++ baekjoon/src/BOJ_2558.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class BOJ_2558 {
+    public static void main(String[] args) throws Exception{
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt()+ sc.nextInt());
+    }
+}
