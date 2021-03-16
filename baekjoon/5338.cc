9f553f010b1fff46b3abd05aee80261c371c468c
Jeong Yo Han <ingyeoking13@gmail.com>
2021-03-16 23:09:12 +0900

    feat(YoHan-Jeong): [BOJ, 5338] 마이크로소프트 로고

---
+++ baekjoon/5338.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  cout << "       _.-;;-._\n'-..-'|   ||   |\n'-..-'|_.-;;-._|\n'-..-'|   ||   |\n'-..-'|_.-''-._|";
+
+}
