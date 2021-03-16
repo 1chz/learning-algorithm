daac6517813a916775ab82ecbf52ae376ed51511
Junsik <wnstlr4045@naver.com>
2021-03-16 16:13:19 +0900

    feat(Junsik-Oh): [BOJ, 18108] 1998년생인 내가 태국에서는 2541년생?!
    https://www.acmicpc.net/problem/18108

---
+++ test/src/bornin1998.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class bornin1998 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        System.out.println(a - 543);
+
+    }
+}
