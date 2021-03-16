ea58192a3a3c0c6a2e66538b0ee5913ae6c40b05
Changhun-Han <hch4102@naver.com>
2021-03-16 21:06:23 +0900

    feat(Changhun-Han): [BOJ, 5543] 상근날드
    https://www.acmicpc.net/problem/5543
    Signed-off-by: Changhun-Han <hch4102@naver.com>

---
+++ java/src/ChanghunHan/baekjoon/BOJ_5543.java
@@ -0,0 +1,38 @@
+package ChanghunHan.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @see <a href="https://www.acmicpc.net/problem/5543">
+ * https://www.acmicpc.net/problem/5543
+ * </a>
+ */
+public class BOJ_5543 {
+    
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int burger = 2001;
+        int drink = 2001;
+        
+        for (int i = 0; i < 3; i++) {
+            int value = Integer.parseInt(br.readLine());
+            if (value < burger) {
+                burger = value;
+            }
+        }
+        
+        for (int i = 0; i < 2; i++) {
+            int value = Integer.parseInt(br.readLine());
+            if (value < drink) {
+                drink = value;
+            }
+        }
+        
+        System.out.println(burger + drink - 50);
+        br.close();
+    }
+    
+}
