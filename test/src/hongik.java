56df34a11d6e8ac6cc174deeeb61a6168ff443f7
Junsik4045 <wnstlr4045@naver.com>
2021-03-16 16:20:32 +0900

    feat(Junsik-Oh): [BOJ, 16394] 홍익대학교
    https://www.acmicpc.net/problem/16394

---
+++ test/src/hongik.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class hongik {
+    public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+
+        System.out.println(a - 1946);
+
+
+    }
+}
