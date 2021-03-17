f2e5898737017f6c0a68e328ab20e809a8d6795a
Junsik4045 <wnstlr4045@naver.com>
2021-03-17 21:35:32 +0900

    feat(Junsik-Oh): [BOJ, 10926 ] ??!
    https://www.acmicpc.net/problem/10926

---
+++ test/src/surprised.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class surprised {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next();
+
+        System.out.println(a + "??!");
+
+
+    }
+}
