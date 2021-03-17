a9c0e666e46ee2a14724c40979fa0a2778e4636e
Changhun-Han <hch4102@naver.com>
2021-03-17 14:31:04 +0900

    feat(Changhun-Han): [BOJ, 2752] 세수정렬
    https://www.acmicpc.net/problem/2752

---
+++ ChanghunHan/src/baekjoon/BOJ_2752.java
@@ -0,0 +1,38 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+import static java.lang.Integer.parseInt;
+
+/**
+ * @see <a href="https://www.acmicpc.net/problem/2752">
+ * https://www.acmicpc.net/problem/2752
+ * </a>
+ */
+public class BOJ_2752 {
+    
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] list = {parseInt(st.nextToken()),
+                      parseInt(st.nextToken()),
+                      parseInt(st.nextToken())};
+        
+        for (int i = 0; i < list.length - 1; i++) {
+            for (int j = 0; j < list.length - 1; j++) {
+                if (list[j] > list[j + 1]) {
+                    int temp = list[j];
+                    list[j] = list[j + 1];
+                    list[j + 1] = temp;
+                }
+            }
+        }
+        
+        for (int i = 0; i < list.length; i++) {
+            System.out.print(list[i] + " ");
+        }
+        br.close();
+    }
+    
+}
