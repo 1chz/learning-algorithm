972090756e7705e11cf60b4e26bac1255f9611d9
Junsik4045 <wnstlr4045@naver.com>
2021-03-17 17:14:41 +0900

    feat(Junsik-Oh): [BOJ, 15727 ] 조별과제를 하려는데 조장이 사라졌다
    https://www.acmicpc.net/problem/15727

---
+++ test/src/teamwork.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class teamwork {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+
+        int b = 5;
+
+        System.out.println((a / b)+1);
+
+
+
+
+
+    }
+}
