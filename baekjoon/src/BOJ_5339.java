42065941e6315b94f546a089226b0f2fc9635817
Gyoungmin-Min <alsrudals933@naver.com>
2021-03-17 10:57:58 +0900

    feat(Gyoungmin-Min):[BOJ,5339] 콜센터 https://www.acmicpc.net/problem/5339

---
+++ baekjoon/src/BOJ_5339.java
@@ -0,0 +1,16 @@
+public class BOJ_5339 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("     /~\\\n");
+        sb.append("    ( oo|\n");
+        sb.append("    _\\=/_\n");
+        sb.append("   /  _  \\\n");
+        sb.append("  //|/.\\|\\\\\n");
+        sb.append(" ||  \\ /  ||\n");
+        sb.append("============\n");
+        sb.append("|          |\n");
+        sb.append("|          |\n");
+        sb.append("|          |\n");
+        System.out.println(sb.toString());
+    }
+}
