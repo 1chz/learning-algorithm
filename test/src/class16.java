30d0bd51adde1e2713c98f4a09ac097d60d5d8e2
Junsik4045 <wnstlr4045@naver.com>
2021-03-17 16:43:41 +0900

    feat(Junsik-Oh): [BOJ, 1550 ] 16진수
    https://www.acmicpc.net/problem/1550

---
+++ test/src/class16.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class class16 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next();
+
+        int b = Integer.parseInt(a, 16);
+
+        System.out.println(b);
+
+    }
+}
