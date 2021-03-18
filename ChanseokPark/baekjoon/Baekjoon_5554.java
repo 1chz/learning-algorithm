e8ad8aaab8eac093edfe16c478ae1e444576f4e3
Park Chan Seok <arenal@naver.com>
2021-03-18 21:35:08 +0900

    feat(Chanseok-Park): [BOJ, 5554] 심부름 가는 길
    https://www.acmicpc.net/problem/5554

--- ChanseokPark/baekjoon/Baekjoon_5554.java
+++ ChanseokPark/baekjoon/Baekjoon_5554.java
@@ -7,7 +7,6 @@ public class Baekjoon_5554 {
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int number = 0;
 
         for(int i=0; i < 4; i++) {
