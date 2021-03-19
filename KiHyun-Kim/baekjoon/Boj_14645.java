7845df2a625998e92ac1a4ad635fd20630aa76ba
bluewow <dogseen@gmail.com>
2021-03-19 19:41:23 +0900

    feat(Kihyun-Kim): [Boj, 14645] 와이버스 부릉부릉

---
+++ KiHyun-Kim/baekjoon/Boj_14645.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Boj_14645 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int len = sc.nextInt();
+        sc.nextInt();
+
+        for(int i = 0; i < len; i++) {
+            sc.nextInt();
+            sc.nextInt();
+        }
+        System.out.println("비와이");
+    }
+}
