0d0b38e442ff8cc724d9ddd555678c5f88d97fa0
Changhun-Han <hch4102@naver.com>
2021-03-20 20:50:29 +0900

    feat(changhun): [BOJ, 10773] 제로
    https://www.acmicpc.net/problem/10773
    Signed-off-by: Changhun-Han <hch4102@naver.com>

---
+++ ChanghunHan/src/baekjoon/bfs_dfs/BOJ_10773.java
@@ -0,0 +1,40 @@
+package baekjoon.bfs_dfs;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+import static java.lang.Integer.parseInt;
+
+/**
+ * @see <a href="https://www.acmicpc.net/problem/10773">
+ * https://www.acmicpc.net/problem/10773
+ * </a>
+ */
+public class BOJ_10773 {
+    
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int K = parseInt(br.readLine());
+        
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < K; i++) {
+            int money = parseInt(br.readLine());
+            if (money == 0) {
+                stack.pop();
+            }
+            else {
+                stack.push(money);
+            }
+        }
+        
+        long sum = 0;
+        while (stack.size() > 0) {
+            sum += stack.pop();
+        }
+        System.out.println(sum);
+        
+    }
+    
+}
