60f81998fb8899db448b86d973e852aae5a2cf53
Changhun-Han <hch4102@naver.com>
2021-03-20 17:57:23 +0900

    feat(changhun): [BOJ, 13023] ABCDE
    https://www.acmicpc.net/problem/13023
    Signed-off-by: Changhun-Han <hch4102@naver.com>

---
+++ ChanghunHan/src/baekjoon/beginner100/BOJ_5339.java
@@ -0,0 +1,25 @@
+package baekjoon.beginner100;
+
+/**
+ * @see <a href="https://www.acmicpc.net/problem/5339">
+ * https://www.acmicpc.net/problem/5339
+ * </a>
+ */
+public class BOJ_5339 {
+    
+    public static void main (String[] args) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("     /~\\\n")
+                     .append("    ( oo|\n")
+                     .append("    _\\=/_\n")
+                     .append("   /  _  \\\n")
+                     .append("  //|/.\\|\\\\\n")
+                     .append(" ||  \\ /  ||\n")
+                     .append("============\n")
+                     .append("|          |\n")
+                     .append("|          |\n")
+                     .append("|          |\n");
+        System.out.println(stringBuilder);
+    }
+    
+}
