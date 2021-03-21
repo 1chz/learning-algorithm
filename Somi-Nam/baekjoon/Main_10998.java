f690a7eafc2c80d78746eb18ebf2eb40b307c8e8
Sumsan38 <ndanl4647@gmail.com>
2021-03-21 15:16:19 +0900

    [백준, 10998] A x B

---
+++ Somi-Nam/baekjoon/Main_10998.java
@@ -0,0 +1,14 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+/**
+ * @author NSM
+ * https://www.acmicpc.net/problem/10998
+ */
+public class Main_10998 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextInt() * sc.nextInt());
+	}
+}
