e2c8f378cff3eb47a0994ef400b76bde7c9fee60
jonny <jonny@bsidesoft.com>
2021-03-21 14:57:54 +0900

    feat(Junhee-Cho): [BOJ, 5522] 카드 게임
    https://www.acmicpc.net/problem/5522

--- JunheeCho/baekjoon/BOJ_14645.java
+++ JunheeCho/baekjoon/BOJ_14645.java
@@ -4,6 +4,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+/**
+ * @see <a href="https://www.acmicpc.net/problem/14645">
+ * https://www.acmicpc.net/problem/14645
+ * </a>
+ */
 public class BOJ_14645 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
