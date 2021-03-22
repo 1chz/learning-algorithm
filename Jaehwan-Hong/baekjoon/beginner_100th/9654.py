dbbb9b2f4b7408f91c7dff7b1f5c5c3036a36da0
jhhong0930 <jhhong0930@gmail.com>
2021-03-22 16:56:55 +0900

    feat(Jaehwan-Hong): [BOJ, 9654] 나부 함대 데이터

---
+++ Jaehwan-Hong/baekjoon/beginner_100th/9654.py
@@ -0,0 +1,7 @@
+# 나부 함대 데이터
+print("SHIP NAME      CLASS          DEPLOYMENT IN SERVICE")
+print("N2 Bomber      Heavy Fighter  Limited    21        ")
+print("J-Type 327     Light Combat   Unlimited  1         ")
+print("NX Cruiser     Medium Fighter Limited    18        ")
+print("N1 Starfighter Medium Fighter Unlimited  25        ")
+print("Royal Cruiser  Light Combat   Limited    4         ")
