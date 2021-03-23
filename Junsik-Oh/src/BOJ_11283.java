3e584defa2b9bab858b338c53f103557aebd2835
Junsik4045 <wnstlr4045@naver.com>
2021-03-23 23:31:04 +0900

    feat(Junsik-Oh): [BOJ, 11283] 한글2
    https://www.acmicpc.net/problem/11283

---
+++ Junsik-Oh/src/BOJ_11283.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BOJ_11283 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next();
+
+
+        char result = a.charAt(0);
+        System.out.println((int)result - 44031);
+
+
+    }
+}
