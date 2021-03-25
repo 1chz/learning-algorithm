763ff501e7bbaf8b7bfa5f2500eeb4eb8777164e
Jeong Yo Han <ingyeoking13@gmail.com>
2021-03-26 01:54:00 +0900

    feat(YoHan-Jeong): [BOJ, 10172] 개

---
+++ JeongYoHan/baekjoon/10172.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  cout << "|\\_/|\n" << "|q p|   /}\n" << "( 0 )\"\"\"\\\n" << "|\"^\"`    |\n" <<
+"||_/=\\\\__|";
+
+}
