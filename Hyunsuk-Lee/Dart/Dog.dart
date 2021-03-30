cbbe9ca3704eaff55b65b0280a085358cc06e51b
hyunsuklee <lhs5323@gmail.com>
2021-03-30 21:29:03 +0900

    file 구조 변경

--- Hyunsuk-Lee/Dart/Dog.dart
+++ Hyunsuk-Lee/Dart/Dog.dart
@@ -1,6 +1,6 @@
+//Dart
 void main(){
   var sB = new StringBuffer();
-  //sB.writeln("|\\_/|\n").writeln("|q p|   /}\n").writeln("( 0 )\"\"\"\\\n").writeln("|\"^\"`    |\n").writeln("||_/=\\\\__|");
   sB.writeAll({'|\\_/|', '|q p|   /}', '( 0 )\"\"\"\\', '|\"^\"`    |', '||_/=\\\\__|'}, '\n');
   print(sB.toString());
 }
