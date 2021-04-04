9ac6987c1008c8736338efeb5ca5da4db854797d
Junsik4045 <wnstlr4045@naver.com>
2021-04-04 16:58:42 +0900

    feat(Junsik-Oh): [BOJ, 13277] 큰 수 곱셉
    https://www.acmicpc.net/problem/13277

---
+++ Junsik-Oh/src/BOJ_13277.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+import java.math.BigInteger;
+
+
+public class BOJ_13277 {
+    public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+
+            BigInteger a = sc.nextBigInteger();
+            BigInteger b = sc.nextBigInteger();
+
+        System.out.println(a.multiply(b));
+
+
+
+
+    }
+}
