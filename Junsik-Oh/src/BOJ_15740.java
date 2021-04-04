e9a42c9835a447ed75607b8fa282dc1104e7818c
Junsik4045 <wnstlr4045@naver.com>
2021-04-04 17:05:42 +0900

    feat(Junsik-Oh): [BOJ, 15740] A+B - 9
    https://www.acmicpc.net/problem/15740

---
+++ Junsik-Oh/src/BOJ_15740.java
@@ -0,0 +1,18 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class BOJ_15740 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        BigInteger a = sc.nextBigInteger();
+        BigInteger b = sc.nextBigInteger();
+
+
+
+        System.out.println(a.add(b));
+
+
+
+    }
+}
