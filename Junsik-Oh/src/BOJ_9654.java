6181737e6a07064dac9565623694e11a69307ea4
Junsik4045 <wnstlr4045@naver.com>
2021-04-04 17:01:53 +0900

    feat(Junsik-Oh): [BOJ, 9654] 나부 함대 데이터
    https://www.acmicpc.net/problem/9654

---
+++ Junsik-Oh/src/BOJ_9654.java
@@ -0,0 +1,12 @@
+public class BOJ_9654 {
+    public static void main(String[] args) {
+        System.out.println("SHIP NAME      CLASS          DEPLOYMENT IN SERVICE");
+        System.out.println("N2 Bomber      Heavy Fighter  Limited    21        ");
+        System.out.println("J-Type 327     Light Combat   Unlimited  1         ");
+        System.out.println("NX Cruiser     Medium Fighter Limited    18        ");
+        System.out.println("N1 Starfighter Medium Fighter Unlimited  25        ");
+        System.out.println("Royal Cruiser  Light Combat   Limited    4         ");
+
+
+    }
+}
