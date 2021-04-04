18e7fafae2d2e1fc7f302dfc885c163a515a3cd1
Changhun-Han <hch4102@naver.com>
2021-04-04 21:50:00 +0900

    feat(Changhun-Han): [BOJ, 3190] 뱀
    https://www.acmicpc.net/problem/3190
    Signed-off-by: Changhun-Han <hch4102@naver.com>

--- Changhun-Han/src/baekjoon/queue/BOJ_3190.java
+++ Changhun-Han/src/baekjoon/queue/BOJ_3190.java
@@ -1,7 +1,11 @@
 package baekjoon.queue;
 
-import java.io.*;
-import java.util.LinkedList;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.StringTokenizer;
 
 import static java.lang.Integer.parseInt;
@@ -13,41 +17,149 @@ import static java.lang.Integer.parseInt;
  */
 public class BOJ_3190 {
     
-    static LinkedList<int[]> snake = new LinkedList<>();
-    static boolean[][] board;
+    /**
+     * Map 구조 <br/>
+     * 0 : 아무것도 없음 <br/>
+     * 1 : 사과 <br/>
+     * 2 : 뱀 <br/>
+     */
+    private static BufferedReader br;
+    private static BufferedWriter bw;
+    private static StringBuilder sb;
     
-    public static void main (String[] args) throws IOException {
+    private static Deque<Point> snake = new ArrayDeque<>();
+    
+    private static int N;
+    private static int K;
+    private static int L;
+    private static int[][] map;
+    private static int[] secondInformation;
+    private static int[] directionInformation;
+    
+    private static int[] dx = {-1, 0, 1, 0};
+    private static int[] dy = {0, 1, 0, -1};
+    
+    public static void main (String[] args) throws Exception {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        sb = new StringBuilder();
         
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        int N = parseInt(br.readLine());
-        int K = parseInt(br.readLine());
-        
-        board = new boolean[N][N];
+        N = parseInt(br.readLine());
+        K = parseInt(br.readLine());
+        map = new int[N + 1][N + 1];
         
         for(int i = 0; i < K; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int row = parseInt(st.nextToken()) - 1;
-            int col = parseInt(st.nextToken()) - 1;
-            board[row][col] = true;
+            int row = parseInt(st.nextToken());
+            int col = parseInt(st.nextToken());
+            map[row][col] = 1;
         }
         
-        int L = parseInt(br.readLine());
+        L = parseInt(br.readLine());
+        secondInformation = new int[L];
+        directionInformation = new int[L];
+        
         for(int i = 0; i < L; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int sec = parseInt(st.nextToken());
-            String dir = st.nextToken();
-            run(sec, dir);
+            secondInformation[i] = parseInt(st.nextToken());
+            directionInformation[i] = getDirection(st.nextToken());
         }
         
+        // TODO: 게임 시작
+        solution();
+        
+        // TODO: 결과 출력 및 리소스 정리
+        bw.write(sb.toString());
         bw.flush();
         bw.close();
         br.close();
     }
     
-    private static void run (int sec, String dir) {
+    private static void solution () {
+        int direction = 1;
+        int second = 0;
+        int result = 0;
+        
+        map[1][1] = 2;
+        snake.add(new Point(1, 1));
+        
+        while(true) {
+            // TODO: 방향 전환
+            if(second < L && secondInformation[second] == result) {
+                direction = (direction + directionInformation[second]) % 4;
+                second++;
+            }
+            
+            // TODO: 머리가 다음칸으로 이동하며 몸통이 늘어남
+            int nextX = snake.getFirst()
+                             .getX() + dx[direction];
+            
+            int nextY = snake.getFirst()
+                             .getY() + dy[direction];
+            
+            // TODO: 머리가 몸통이나 벽과 부딪히면 종료
+            if(nextX <= 0 || nextX > N || nextY <= 0 || nextY > N) {
+                sb.append(result + 1);
+                break;
+            }
+            
+            if(map[nextX][nextY] == 2) {
+                sb.append(result + 1);
+                break;
+            }
+            
+            // TODO: 이동한 칸에 사과가 있다면, 사과를 먹고 꼬리를 유지
+            if(map[nextX][nextY] == 1) {
+                headForward(nextX, nextY);
+            }
+            
+            // TODO: 이동한 칸에 사과가 없다면, 꼬리를 자름
+            else if(map[nextX][nextY] == 0) {
+                headForward(nextX, nextY);
+                removeTail();
+            }
+            
+            result++;
+        }
+    }
     
+    private static int getDirection (String s) {
+        if("D".equals(s)) {
+            return 1;
+        }
+        else {
+            return 3;
+        }
+    }
+    
+    private static void headForward (int nextX, int nextY) {
+        map[nextX][nextY] = 2;
+        snake.addFirst(new Point(nextX, nextY));
+    }
+    
+    private static void removeTail () {
+        Point tail = snake.removeLast();
+        map[tail.getX()][tail.getY()] = 0;
+    }
+    
+    private static class Point {
+        
+        private int x;
+        private int y;
+        
+        private Point (int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+        
+        private int getX () {
+            return x;
+        }
+        
+        private int getY () {
+            return y;
+        }
+        
     }
     
 }
