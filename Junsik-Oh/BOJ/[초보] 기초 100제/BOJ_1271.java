1845582a4196ba184827b41b069bb1690520265c
Junsik4045 <wnstlr4045@naver.com>
2021-04-04 17:18:32 +0900

    feat(Junsik-Oh): [BOJ, 1271] 엄청난 부자2
    https://www.acmicpc.net/problem/1271

---
+++ Junsik-Oh/BOJ/[초보] 기초 100제/BOJ_1271.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BOJ_1271 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a / b);
+        System.out.println(a % b);
+
+
+    }
+}
