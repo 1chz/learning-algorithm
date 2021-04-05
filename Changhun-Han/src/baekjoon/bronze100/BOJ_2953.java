83c7ba21307c8864cd128dc908255357f70fe8d8
Changhun-H <hch4102@hanpass.com>
2021-04-05 17:24:44 +0900

    feat(Changhun-Han): [BOJ, 10951] A+B - 4
    https://www.acmicpc.net/problem/10951

--- Changhun-Han/src/baekjoon/bronze100/BOJ_2953.java
+++ Changhun-Han/src/baekjoon/bronze100/BOJ_2953.java
@@ -1,9 +1,13 @@
 package baekjoon.bronze100;
 
-import java.io.*;
-import java.util.StringTokenizer;
-
 import static java.lang.Integer.parseInt;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 /**
  * @see <a href="https://www.acmicpc.net/problem/2953">
@@ -13,7 +17,6 @@ import static java.lang.Integer.parseInt;
 public class BOJ_2953 {
     
     public static void main (String[] args) throws IOException {
-        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
