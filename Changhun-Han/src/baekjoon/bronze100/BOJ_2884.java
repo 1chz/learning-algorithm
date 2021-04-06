f3d3f044d5aab646bc9624d447a23aded726b0a3
Changhun-Han <hch4102@naver.com>
2021-04-06 20:14:24 +0900

    feat(Changhun-Han): [BOJ, 2884] 알람 시계
    https://www.acmicpc.net/problem/2884
    Signed-off-by: Changhun-Han <hch4102@naver.com>

--- Changhun-Han/src/baekjoon/bronze100/BOJ_2884.java
+++ Changhun-Han/src/baekjoon/bronze100/BOJ_2884.java
@@ -14,7 +14,27 @@ import static java.lang.Integer.parseInt;
  */
 public class BOJ_2884 {
     
-    public static void main (String[] args) throws IOException {
+    public static void solution1 () throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int H = parseInt(st.nextToken());
+        int M = parseInt(st.nextToken());
+    
+        LocalTime time = LocalTime.of(H, M)
+                                  .minusMinutes(45);
+    
+        String result = DateTimeFormatter.ofPattern("H m")
+                                         .format(time);
+    
+        bw.write(result);
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+    
+    public static void solution2 () throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
@@ -22,13 +42,17 @@ public class BOJ_2884 {
         int H = parseInt(st.nextToken());
         int M = parseInt(st.nextToken());
         
-        LocalTime time = LocalTime.of(H, M)
-                                  .minusMinutes(45);
+        M -= 45;
         
-        String result = DateTimeFormatter.ofPattern("H m")
-                                         .format(time);
+        if(M < 0) {
+            M += 60;
+            H -= 1;
+            if(H < 0) {
+                H = 23;
+            }
+        }
         
-        bw.write(result);
+        bw.write(H + " " + M);
         bw.flush();
         bw.close();
         br.close();
