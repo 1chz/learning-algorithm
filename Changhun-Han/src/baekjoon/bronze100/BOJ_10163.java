98d4da987c24e46e159f1bfceb91c1b60a80573a
Changhun-H <hch4102@hanpass.com>
2021-04-06 10:08:11 +0900

    feat(Changhun-Han): [BOJ, 10163] 색종이
    https://www.acmicpc.net/problem/10163

---
+++ Changhun-Han/src/baekjoon/bronze100/BOJ_10163.java
@@ -0,0 +1,63 @@
+package baekjoon.bronze100;
+
+import static java.lang.Integer.parseInt;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+/**
+ * @see <a href="https://www.acmicpc.net/problem/10163">
+ * https://www.acmicpc.net/problem/10163
+ * </a>
+ */
+public class BOJ_10163 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        int N = parseInt(br.readLine());
+
+        int[][] map = new int[101][101];
+
+        for (int i = 1; i <= N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = parseInt(st.nextToken());
+            int y = parseInt(st.nextToken());
+            int width = parseInt(st.nextToken());
+            int height = parseInt(st.nextToken());
+            for (int j = x; j < x + width; j++) {
+                for (int k = y; k < y + height; k++) {
+                    map[j][k] = i;
+                }
+            }
+        }
+
+        for (int i = 1; i <= N; i++) {
+            int area = 0;
+            for (int j = 0; j < 101; j++) {
+                for (int k = 0; k < 101; k++) {
+                    if (map[j][k] == i) {
+                        area++;
+                    }
+                }
+            }
+            if (area == 0) {
+                sb.append(0 + "\n");
+            } else {
+                sb.append(area + "\n");
+            }
+        }
+
+        bw.write(sb.toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
