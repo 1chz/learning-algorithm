1baf1e1df0797657e592feeab6ab6d0fa6cdc701
Changhun-H <hch4102@hanpass.com>
2021-04-06 15:09:16 +0900

    feat(Changhun-Han): [BOJ, 1157] 단어 공부
    https://www.acmicpc.net/problem/1157

---
+++ Changhun-Han/src/baekjoon/bronze100/BOJ_1157.java
@@ -0,0 +1,47 @@
+package baekjoon.bronze100;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+/**
+ * @see <a href="https://www.acmicpc.net/problem/1157">
+ * https://www.acmicpc.net/problem/1157
+ * </a>
+ */
+public class BOJ_1157 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[] chars = new int[26];
+        int c = System.in.read();
+
+        while (c > 64) {
+            if (c < 91) {
+                chars[c - 65]++;
+            }
+            else {
+                chars[c - 97]++;
+            }
+            c = System.in.read();
+        }
+
+        int max = -1;
+        int ch = -2;
+
+        for (int i = 0; i < 26; i++) {
+            if (chars[i] > max) {
+                max = chars[i];
+                ch = i;
+            }
+            else if (chars[i] == max) {
+                ch = -2;
+            }
+        }
+        bw.write((char) (ch + 65));
+        bw.flush();
+        bw.close();
+    }
+
+}
