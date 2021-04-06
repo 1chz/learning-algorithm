f297a35f150c5175378e7c6228b5175306453422
Changhun-H <hch4102@hanpass.com>
2021-04-06 10:01:13 +0900

    feat(Changhun-Han): [BOJ, 1546] 평균
    https://www.acmicpc.net/problem/1546

---
+++ Changhun-Han/src/baekjoon/bronze100/BOJ_1546.java
@@ -0,0 +1,47 @@
+package baekjoon.bronze100;
+
+import static java.lang.Integer.parseInt;
+import static java.lang.String.valueOf;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+/**
+ * @see <a href="https://www.acmicpc.net/problem/1546">
+ * https://www.acmicpc.net/problem/1546
+ * </a>
+ */
+public class BOJ_1546 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = parseInt(br.readLine());
+
+        int[] scores = Stream.of(br.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int max = Arrays.stream(scores)
+                .max()
+                .getAsInt();
+
+        int sum = Arrays.stream(scores)
+                .sum();
+
+        double calculate = (double) max / 100;
+        double avg = sum / calculate / N;
+
+        bw.write(valueOf(avg));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
