9bdaf7b6f4790035c55c0b7483f27f19a14187da
Changhun-Han <hch4102@naver.com>
2021-04-07 19:33:31 +0900

    feat(Changhun-Han): [BOJ, 10757] 큰 수 A+B
    https://www.acmicpc.net/problem/10757
    Signed-off-by: Changhun-Han <hch4102@naver.com>

---
+++ Changhun-Han/src/baekjoon/bronze100/BOJ_10757.java
@@ -0,0 +1,31 @@
+package baekjoon.bronze100;
+
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+import static java.lang.Integer.parseInt;
+import static java.lang.String.valueOf;
+
+/**
+ * @see <a href="https://www.acmicpc.net/problem/10757">
+ * https://www.acmicpc.net/problem/10757
+ * </a>
+ */
+public class BOJ_10757 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        BigInteger A = new BigInteger(st.nextToken());
+        BigInteger B = new BigInteger(st.nextToken());
+        
+        bw.write(A.add(B).toString());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+}
