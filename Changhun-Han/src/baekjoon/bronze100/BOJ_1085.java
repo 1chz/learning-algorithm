34efae8a132339c17fe60a418a4b9668bceb861b
Changhun-Han <hch4102@naver.com>
2021-04-08 20:04:28 +0900

    feat(Changhun-Han): [BOJ, 1085] 직사각형에서 탈출
    https://www.acmicpc.net/problem/1085
    Signed-off-by: Changhun-Han <hch4102@naver.com>

---
+++ Changhun-Han/src/baekjoon/bronze100/BOJ_1085.java
@@ -0,0 +1,33 @@
+package baekjoon.bronze100;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+import static java.lang.Integer.parseInt;
+import static java.lang.Math.min;
+import static java.lang.String.valueOf;
+
+/**
+ * @see <a href="https://www.acmicpc.net/problem/1085">
+ * https://www.acmicpc.net/problem/1085
+ * </a>
+ */
+public class BOJ_1085 {
+    
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int X = parseInt(st.nextToken());
+        int Y = parseInt(st.nextToken());
+        int W = parseInt(st.nextToken());
+        int H = parseInt(st.nextToken());
+        
+        bw.write(valueOf(min(min(X, W - X), min(Y, H - Y))));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+    
+}
