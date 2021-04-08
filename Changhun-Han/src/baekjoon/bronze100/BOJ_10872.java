5fe8cad6eb21801c42d7e37e0d4d186855637246
Changhun-Han <hch4102@naver.com>
2021-04-08 20:21:23 +0900

    feat(Changhun-Han): [BOJ, 10872] 팩토리얼
    https://www.acmicpc.net/problem/10872
    Signed-off-by: Changhun-Han <hch4102@naver.com>

---
+++ Changhun-Han/src/baekjoon/bronze100/BOJ_10872.java
@@ -0,0 +1,34 @@
+package baekjoon.bronze100;
+
+import java.io.*;
+
+import static java.lang.Integer.parseInt;
+import static java.lang.String.valueOf;
+
+/**
+ * @see <a href="https://www.acmicpc.net/problem/10872">
+ * https://www.acmicpc.net/problem/10872
+ * </a>
+ */
+public class BOJ_10872 {
+    
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int sum = 1;
+        
+        int N = parseInt(br.readLine());
+        
+        while(0 != N) {
+            sum *= N;
+            N--;
+        }
+        
+        bw.write(valueOf(sum));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+    
+}
