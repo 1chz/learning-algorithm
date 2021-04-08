fcf80884454b1791c539d06a6e9122c5cb61c8e6
Changhun-Han <hch4102@naver.com>
2021-04-08 20:39:30 +0900

    feat(Changhun-Han): [BOJ, 2231] 분해합
    https://www.acmicpc.net/problem/2231
    Signed-off-by: Changhun-Han <hch4102@naver.com>

---
+++ Changhun-Han/src/baekjoon/bronze100/BOJ_2231.java
@@ -0,0 +1,48 @@
+package baekjoon.bronze100;
+
+import java.io.*;
+
+import static java.lang.Integer.parseInt;
+import static java.lang.String.valueOf;
+
+/**
+ * @see <a href="https://www.acmicpc.net/problem/2231">
+ * https://www.acmicpc.net/problem/2231
+ * </a>
+ */
+public class BOJ_2231 {
+    
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        String s = br.readLine();
+        
+        int result = 0;
+        
+        int N = parseInt(s);
+        int len = s.length();
+        
+        for(int i = (N - (len * 9)); i < N; i++) {
+            int num = i;
+            int sum = 0;
+            
+            while(num != 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+            
+            if(sum + i == N) {
+                result = i;
+                break;
+            }
+            
+        }
+        
+        bw.write(valueOf(result));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+    
+}
