cdfa109eb9a4dcc03bb3fe77ea9672a8c4c35a12
Changhun-Han <hch4102@naver.com>
2021-04-10 22:47:39 +0900

    [2021-04-10] 4주차 - 이탈자 폴더 정리
    start point
    
    Signed-off-by: Changhun-Han <hch4102@naver.com>

--- Hyunsuk-Lee/Dart/Quadrant.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:io';
-//dart
-void main(){
-  var x = int.parse(stdin.readLineSync());
-  var y = int.parse(stdin.readLineSync());
-  var quadrant = (x > 0) ? ((y > 0) ? 1 : 4) : ((y > 0) ? 2 : 3);
-  stdout.write(quadrant);
-}
