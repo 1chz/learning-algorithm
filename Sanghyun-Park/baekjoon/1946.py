d4d0cf023a7d1fc79ca7637387fdc589a37680d2
PPakSang <ppm5377@naver.com>
2021-04-10 20:28:14 +0900

    feat(<branch>): [<BOJ>, <1946>] <신입사원>
    https://www.acmicpc.net/problem/1946

--- Sanghyun-Park/baekjoon/1946.py
+++ Sanghyun-Park/baekjoon/1946.py
@@ -1,29 +1,31 @@
 import sys
-input = sys.stdin.readline
-t = int(input())
+
+
+t = int(sys.stdin.readline())
 for i in range(t):
-    n = int(input())
-    s = [0 for i in range(n + 1)]
-    for j in range(n):
-        a, b = map(int, input().split())
+
+    N1 = int(sys.stdin.readline())
+    s = [0 for i in range(N1 + 1)]
+    for j in range(N1):
+        a, b = map(int, sys.stdin.readline().split())
         s[a] = b
     min_n = s[1]
     cnt = 0
-    for k in range(2, n + 1):
+    for k in range(2, N1 + 1):
         if s[k] > min_n:
             cnt += 1
         else:
             min_n = s[k]
-    print(n - cnt)
+    print(N1 - cnt)
 
-
-import sys
-
-t= int(input())
-
-for _ in range(t):
-    n=int(input())
-    lst=sorted([list(map(int,sys.stdin.readline().strip().split())) for x in range(n)],key= lambda x:x[0])
-
-    print(lst)
+#
+# import sys
+#
+# t= int(input())
+#
+# for _ in range(t):
+#     n=int(input())
+#     lst=sorted([list(map(int,sys.stdin.readline().strip().split())) for x in range(n)],key= lambda x:x[0])
+#
+#     print(lst)
 
