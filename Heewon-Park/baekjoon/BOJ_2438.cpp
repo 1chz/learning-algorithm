97531e53697eeb15f4846bc447c870bd43c97610
7198hee <7198hee@gmail.com>
2021-04-15 22:53:05 +0900

    feat(Heewon-Park): [BOJ, 2438] 별 찍기 - 1 https://www.acmicpc.net/problem/2438

---
+++ Heewon-Park/baekjoon/BOJ_2438.cpp
@@ -0,0 +1,18 @@
+// 별 찍기 - 1
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i=1; i<=n; i++){
+        for (int j=0; j<i; j++){
+            cout << '*';
+        }
+        cout << '\n';
+    }
+   
+   return 0;
+}
