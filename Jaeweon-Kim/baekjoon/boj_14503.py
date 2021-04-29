d6432b0739f77a7a66a49fc39629685cb5a88942
Jaeweon Kim <heyjaekim@gmail.com>
2021-04-29 18:36:15 +0900

    feat(Jaeweon-Kim): [boj, 19326] 청소년상어

--- Jaeweon-Kim/baekjoon/boj_14503.py
+++ Jaeweon-Kim/baekjoon/boj_14503.py
@@ -4,8 +4,10 @@ from collections import deque
 N, M = map(int, input().split())
 x, y, d = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(N)]
+# 북 동 남 서
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
+# x,y 좌표에 로봇 위치 2로 지정
 board[x][y] = 2
 result = 1
 
@@ -14,7 +16,7 @@ def bfs(x, y, d):
     while True:
         check = False
         for i in range(4):
-            d = (d + 3) % 4
+            d = (d+3) % 4
             nx, ny = x + dx[d], y + dy[d]
             
             if board[nx][ny] == 0:
@@ -25,7 +27,7 @@ def bfs(x, y, d):
                 break
 
         if not check:
-            if board[x-dx[d]][y-dy[d]] == 1:
+            if board[x - dx[d]][y - dy[d]] == 1:
                 return
             else:
                 x, y = x - dx[d], y - dy[d]
