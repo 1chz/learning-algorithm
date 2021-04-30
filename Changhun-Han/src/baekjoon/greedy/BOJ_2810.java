ef4bbcedc7d5e2df92e4e83f66a05313cfee1520
shirohoo <hch4102@naver.com>
2021-04-30 17:17:41 +0900

    feat(Changhun-Han): [BOJ, 2810] 컵홀더
    https://www.acmicpc.net/problem/2810
    Signed-off-by: shirohoo <hch4102@naver.com>

---
+++ Changhun-Han/src/baekjoon/greedy/BOJ_2810.java
@@ -0,0 +1,44 @@
+package baekjoon.greedy;
+
+import static java.lang.Integer.parseInt;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @see <a href="https://www.acmicpc.net/problem/2810">
+ * https://www.acmicpc.net/problem/2810
+ * </a>
+ */
+public class BOJ_2810 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = parseInt(br.readLine());
+        String s = br.readLine();
+
+        int L = 0;
+        int cupHolder = 0;
+
+        for (int i = 0; i < N; i++) {
+            if (s.charAt(i) == 'S') {
+                cupHolder++;
+            } else {
+                L++;
+                if (L == 2) {
+                    cupHolder++;
+                    L = 0;
+                }
+            }
+        }
+
+        int ans = 2 + (cupHolder - 1) > N
+                ?
+                N : 2 + (cupHolder - 1);
+
+        System.out.println(ans);
+    }
+
+}
