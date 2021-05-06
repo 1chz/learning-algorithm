bbd558b65c32ed1651cb17cefaea5394f86e3983
Soojung Kim <soojung.kim@bespinglobal.com>
2021-05-06 23:44:47 +0900

    feat(Soojung-Kim) : [BOJ, 13277] 큰 수 곱셈

---
+++ Soojung-Kim/baekjoon/BOJ_13277.java
@@ -0,0 +1,19 @@
+package baekjoon;
+
+import java.io.*;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class BOJ_13277 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str, " ");
+
+        BigInteger a = new BigInteger(st.nextToken());
+        BigInteger b = new BigInteger(st.nextToken());
+        System.out.println(a.multiply(b));
+        
+        br.close();
+    }
+}
