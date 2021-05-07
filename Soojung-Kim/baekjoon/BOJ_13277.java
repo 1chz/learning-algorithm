533a7597b80d801fed50a1d51ae18ebae56ed04b
hailey21 <hailey.soojung@gmail.com>
2021-05-07 10:51:48 +0900

    feat(Soojung-Kim) : 인증 커밋 테스트

--- Soojung-Kim/baekjoon/BOJ_13277.java
+++ Soojung-Kim/baekjoon/BOJ_13277.java
@@ -6,7 +6,6 @@ import java.util.StringTokenizer;
 
 public class BOJ_13277 {
     public static void main(String[] args) throws IOException {
-        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = br.readLine();
         StringTokenizer st = new StringTokenizer(str, " ");
