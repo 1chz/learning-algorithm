f1addb965b24259457f8f6ef8272ffc16ab697ad
shirohoo <hch4102@naver.com>
2021-05-22 16:23:19 +0900

    feat(develop): 10주차 이탈자 정리
     - 총원 : 14
     - 제출 : 12
     - 미제출 : 2
     - 통과 : 13
    
     - 한문제 이상 커밋이력 확인되나 PR 관련 문제
        i. 송영섭 (PS확인 / PR 미제출)
    
     - 이탈자 - 일주일 간 단 한 문제도 풀지 않은 사람
        i. 김재원
    
    Signed-off-by: shirohoo <hch4102@naver.com>

--- Jaeweon-Kim/baekjoon/boj_11058.py
+++
@@ -1,9 +0,0 @@
-# 크리보드
-n = int(input())
-dp = [i for i in range(n+1)]
-if n <= 6:
-    print(dp[n])
-else:
-    for i in range(7, n+1):
-        dp[i] = max(dp[i-3] * 2, max(dp[i-4] * 3, dp[i-5] * 4))
-    print(dp[n])
