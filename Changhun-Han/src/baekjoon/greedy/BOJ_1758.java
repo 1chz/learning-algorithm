b3afde97454eaa5b4101a824cb1410c814777b1e
shirohoo <hch4102@naver.com>
2021-05-29 18:56:29 +0900

    feat(Changhun-Han): [BOJ, 1758] 알바생 강호
    https://www.acmicpc.net/problem/1758
    Signed-off-by: shirohoo <hch4102@naver.com>

---
+++ Changhun-Han/src/baekjoon/greedy/BOJ_1758.java
@@ -0,0 +1,41 @@
+package baekjoon.greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+
+import static java.lang.Integer.parseInt;
+import static java.util.Arrays.sort;
+
+/**
+ * @see <a href="https://www.acmicpc.net/problem/1758">
+ * https://www.acmicpc.net/problem/1758
+ * </a>
+ */
+public class BOJ_1758 {
+    
+    public static void main(String[] args) throws IOException {
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            int N = parseInt(br.readLine());
+            Integer[] tips = new Integer[N];
+            
+            for(int i = 0; i < N; i++) {
+                tips[i] = parseInt(br.readLine());
+            }
+            
+            sort(tips, Collections.reverseOrder());
+            
+            long sum = 0;
+            for(int i = 0; i < N; i++) {
+                int tip = tips[i] - ((i + 1) - 1);
+                if(tip <= 0) {
+                    break;
+                }
+                sum += tip;
+            }
+            System.out.println(sum);
+        }
+    }
+    
+}
