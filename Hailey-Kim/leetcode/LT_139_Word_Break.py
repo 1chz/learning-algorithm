4bd4324a369474c2cc95c2087f988b31f0b8db79
haileykr <41278804+haileykr@users.noreply.github.com>
2021-06-01 12:14:16 +0900

    (feat):Hailey-Kim [LT, 139]Word Break
    https://leetcode.com/problems/word-break/

--- Hailey-Kim/leetcode/LT_139_Word_Break.py
+++ Hailey-Kim/leetcode/LT_139_Word_Break.py
@@ -1,55 +1,17 @@
 '''
-208. Implement Trie (Prefix Tree)
+Word Break
 
-https://leetcode.com/problems/implement-trie-prefix-tree/
-05/31/2021
-
+06/01/2021
+https://leetcode.com/problems/word-break/
 '''
-
-class Trie:
-    def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        self.head = {}
+class Solution:
+	def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        n = len(s)
+        dp = [False for i in range(n+1)]
+        dp[0] = True
         
-    def insert(self, word: str) -> None:
-        """
-        Inserts a word into the trie.
-        """
-        cur = self.head
-        for ch in word:
-            if ch not in cur:
-                cur[ch] = {}
-            cur = cur[ch]
-        cur['*'] = True
-        
-    def search(self, word: str) -> bool:
-        """
-        Returns if the word is in the trie.
-        """
-        cur = self.head
-        for ch in word:
-            if ch not in cur:
-                return False
-            cur = cur[ch]
-        if '*' in cur:
-            return True
-        return False
-        
-    def startsWith(self, prefix: str) -> bool:
-        """
-        Returns if there is any word in the trie that starts with the given prefix.
-        """
-        cur = self.head
-        for ch in prefix:
-            if ch not in cur:
-                return False
-            cur = cur[ch]
-        return True
-        
-# Your Trie object will be instantiated and called as such:
-# obj = Trie()
-# obj.insert(word)
-# param_2 = obj.search(word)
-# param_3 = obj.startsWith(prefix)
+        for i in range(1,n+1): # 각 문자열의 문자에서
+            for w in wordDict: # wordDict의 모든 문자열을 검사해줌
+                if dp[i-len(w)] and s[i-len(w):i]==w: #문자열의 시작이 True이고 해당 문자열이 포함되면
+                    dp[i]=True
+        return dp[-1]
