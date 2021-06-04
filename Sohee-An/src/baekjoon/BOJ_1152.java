918938074ba0966cbb7241f1f91a14cc6f253dd0
soheeoott <soheeoott@gmail.com>
2021-06-05 05:28:49 +0900

    feat(Sohee-An): [BOJ, 1152] 단어의 개수

---
+++ Sohee-An/src/baekjoon/BOJ_1152.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String word = br.readLine();
+        br.close();
+
+        StringTokenizer st = new StringTokenizer(word," ");
+        System.out.print(st.countTokens());
+    }
+}
