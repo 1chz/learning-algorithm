f8733ca74fd67385cf1cb21e53d7f83d79cf80b0
siro <71188307+shirohoo@users.noreply.github.com>
2021-06-12 13:41:28 +0900

    Weekly Pull Request - 한창훈 (#315)
    * feat(Changhun-Han)[Programmers, 42586] 기능개발
    
    https://programmers.co.kr/learn/courses/30/lessons/42586?language=java
    Signed-off-by: shirohoo <hch4102@naver.com>
    
    * feat(Changhun-Han)[BOJ, 5585] 거스름돈
    
    https://www.acmicpc.net/problem/5585
    Signed-off-by: shirohoo <hch4102@naver.com>
    
    * feat(Changhun-Han)DataStructure, ArrayList 구현
    
    Signed-off-by: shirohoo <hch4102@naver.com>
    
    * feat(Changhun-Han): [BOJ, 1406] 에디터
    
    https://www.acmicpc.net/problem/1406
    Signed-off-by: shirohoo <hch4102@naver.com>
    
    * feat(Changhun-Han): [BOJ, 1926] 그림
    
    https://www.acmicpc.net/problem/1926
    Signed-off-by: shirohoo <hch4102@naver.com>
    
    * feat(Changhun-Han): [BOJ, 1926] 그림
    
    https://www.acmicpc.net/problem/1926
    Signed-off-by: shirohoo <hch4102@naver.com>
    
    * feat(Changhun-Han): [BOJ, 2217] 로프
    
    https://www.acmicpc.net/problem/2217
    Signed-off-by: shirohoo <hch4102@naver.com>
    
    * feat(Changhun-Han): [BOJ, 1343] 폴리오미노
    
    https://www.acmicpc.net/problem/1343
    Signed-off-by: shirohoo <hch4102@naver.com>
    
    * feat(Changhun-Han): [BOJ, 1758] 알바생 강호
    
    https://www.acmicpc.net/problem/1758
    Signed-off-by: shirohoo <hch4102@naver.com>
    
    * feat(Changhun-Han): [BOJ, 11399] ATM
    
    https://www.acmicpc.net/problem/11399
    Signed-off-by: shirohoo <hch4102@naver.com>
    
    * feat(Changhun-Han): [BOJ, 11399] ATM
    
    https://www.acmicpc.net/problem/11399
    Signed-off-by: shirohoo <hch4102@naver.com>
    
    * feat(Changhun-Han): [BOJ, 11508] 2+1 세일
    
    https://www.acmicpc.net/problem/11508
    Signed-off-by: shirohoo <hch4102@naver.com>
    
    * feat(Changhun-Han): [BOJ, 20115] 에너지 드링크
    
    https://www.acmicpc.net/problem/20115
    Signed-off-by: siro <hch4102@naver.com>

---
+++ Changhun-Han/src/baekjoon/greedy/BOJ_20115.java
@@ -0,0 +1,37 @@
+package baekjoon.greedy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import static java.lang.Integer.parseInt;
+import static java.util.Arrays.stream;
+import static java.util.Collections.reverseOrder;
+
+/**
+ * @see <a href="https://www.acmicpc.net/problem/20115">
+ * https://www.acmicpc.net/problem/20115
+ * </a>
+ */
+public class BOJ_20115 {
+    
+    public static void main(String[] args) throws IOException {
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            int N = parseInt(br.readLine());
+            
+            Double[] drinks = stream(br.readLine().split(" "))
+                    .map(Double::parseDouble)
+                    .sorted(reverseOrder())
+                    .toArray(Double[]::new);
+            
+            double result = drinks[0];
+            for(int i = 1; i < N; i++) {
+                result += drinks[i] / 2;
+            }
+            
+            System.out.print(result);
+            
+        }
+    }
+    
+}
