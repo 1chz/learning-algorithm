9ae3ad211e61959fa9ee7b2a92ba04f555f3107c
siro <hch4102@naver.com>
2021-06-12 13:52:00 +0900

    feat(develop): 이탈자 정리
    Signed-off-by: siro <hch4102@naver.com>

--- Yesong-Oh/leetcode/add-strings.js
+++
@@ -1,3 +0,0 @@
-var addStrings = function(num1, num2) {
-    return String(BigInt(num1) + BigInt(num2));
-};
--- Yesong-Oh/leetcode/missing-number.js
+++
@@ -1,4 +0,0 @@
-var missingNumber = function(nums) {
-    const n = nums.length;
-    return n*(n+1)/2 - nums.reduce((a, c) => a + c);
-};
