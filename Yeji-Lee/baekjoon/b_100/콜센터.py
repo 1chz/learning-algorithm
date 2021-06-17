3c60915a6d21c0f07e805b4aac40a2fbd7e7d9cd
LEEYEJI501 <yeaji0024@gmail.com>
2021-06-17 09:16:26 +0900

    feat(Yeji-Lee): [BOJ, 5339] 콜센터 https://www.acmicpc.net/problem/5339

---
+++ Yeji-Lee/baekjoon/b_100/콜센터.py
@@ -0,0 +1,12 @@
+# https://www.acmicpc.net/problem/5339
+
+print("     /~\\")
+print("    ( oo|")
+print("    _\=/_")
+print("   /  _  \\")
+print("  //|/.\|\\\\")
+print(" ||  \ /  ||")
+print("============")
+print("|          |")
+print("|          |")
+print("|          |")
