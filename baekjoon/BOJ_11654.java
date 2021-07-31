4f904b110714ab3142c2d1c6cd829262d4ab5011
yungc97 <80386631+yungc97@users.noreply.github.com>
2021-07-31 16:39:32 +0900

    Weekly Pull Request - 최연지 (#374)
    * feat(YeonJi-Choi): [BOJ, 2562] 최댓값
    https://www.acmicpc.net/problem/2562
    
    * feat(YeonJi-Choi): [BOJ, 2588] 곱셈
    https://www.acmicpc.net/problem/2588
    
    * feat(YeonJi-Choi): [BOJ, 2742] 곱셈
    https://www.acmicpc.net/problem/2742
    
    * feat(YeonJi-Choi): [BOJ, 2742] 윤년
    https://www.acmicpc.net/problem/2753
    
    * feat(YeonJi-Choi): [BOJ, 5543] 상근날드
    https://www.acmicpc.net/problem/5543
    
    * feat(YeonJi-Choi): [BOJ, 10172] 개
    https://www.acmicpc.net/problem/10172
    
    * feat(YeonJi-Choi): [BOJ, 10797] 10부제
    https://www.acmicpc.net/problem/10797
    
    * feat(YeonJi-Choi): [BOJ, 10817] 세 수
    https://www.acmicpc.net/problem/10817
    
    * feat(YeonJi-Choi): [BOJ, 11022] A+B - 8
    https://www.acmicpc.net/problem/11022
    
    * feat(YeonJi-Choi): [BOJ, 11654] 아스키 코드
    https://www.acmicpc.net/problem/11654
    
    * feat(YeonJi-Choi): [BOJ, 14681] 사분면 고르기
    https://www.acmicpc.net/problem/14681

---
+++ baekjoon/BOJ_11654.java
@@ -0,0 +1,13 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class BOJ_11654 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println((int)sc.next().charAt(0));
+	}
+}
