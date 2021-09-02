7714ab2d5f949155b9443d06452271e71a73e8d3
yungc97 <2__nd@naver.com>
2021-09-02 21:38:35 +0900

    feat(Yeonji-Choi): [BOJ, 13277] 큰 수 곱셈 acmicpc.net/problem/13277

---
+++ baekjoon/BOJ_13277.java
@@ -0,0 +1,27 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class BOJ_13277 {
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		BigInteger a = new BigInteger(st.nextToken());
+		BigInteger b = new BigInteger(st.nextToken());
+		
+		System.out.println(a.multiply(b));
+		
+		br.close();
+	}
+
+}
