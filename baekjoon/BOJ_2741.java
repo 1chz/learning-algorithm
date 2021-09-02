57e099bab5d0b923fe5b260e7e1fa5ef441df5f6
yungc97 <2__nd@naver.com>
2021-09-02 21:39:52 +0900

    feat(Yeonji-Choi): [BOJ, 15552] N 찍기 acmicpc.net/problem/15552

---
+++ baekjoon/BOJ_2741.java
@@ -0,0 +1,19 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class BOJ_2741 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		for(int i=1; i<=n; i++) {
+			System.out.println(i);
+		}
+		
+	}
+
+}
