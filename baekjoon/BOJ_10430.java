fead8f03d7e81468740d7ed0d58bfa01fd86cc0e
yungc97 <2__nd@naver.com>
2021-09-02 21:37:15 +0900

    feat(Yeonji-Choi): [BOJ, 10430] 나머지 acmicpc.net/problem/10430

---
+++ baekjoon/BOJ_10430.java
@@ -0,0 +1,29 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class BOJ_10430 {
+	
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		
+		bw.write((a+b)%c + "\n" + ((a%c) + (b%c))%c + "\n" + (a*b)%c + "\n" + ((a%c) * (b%c))%c);
+		
+		br.close();
+		bw.close();
+	}
+
+}
