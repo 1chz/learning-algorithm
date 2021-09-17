a1f0edac225ffa0b02ad3a7b941905bd271859c9
seongit <seongeunk102@gmail.com>
2021-09-17 14:04:46 +0900

    feat(Seongeun-Kim):[BOJ,10950] A+B -3 https://www.acmicpc.net/problem/10950

---
+++ Seongeun-Kim/src/baekjoon/BOJ_10950.java
@@ -0,0 +1,12 @@
+package baekjoon;
+
+public class BOJ_10950 {
+
+ static Scanner sc = new Scanner (System.in);
+ public static void main(String[] args) {
+     int T = sc.nextInt(); for(int i=1;i<=T;i++)
+     {
+         int a = sc.nextInt(); int b = sc.nextInt(); System.out.println(a+b);
+     }
+ }
+}
