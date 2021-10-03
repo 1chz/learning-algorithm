8c85f432cd709d9a6e770aa5c406be68907c96c2
Changhun Han <71188307+shirohoo@users.noreply.github.com>
2021-10-03 10:13:42 +0900

    Weekly Pull Request - 한창훈 (#419)
    * feat(Changhun-Han)[Programmers, 42586] 기능개발
    
    https://programmers.co.kr/learn/courses/30/lessons/42586?language=java
    Signed-off-by: shirohoo <hch4102@naver.com>
    
    * feat(Changhun-Han)[BOJ, 5585] 거스름돈
    
    https://www.acmicpc.net/problem/5585
    Signed-off-by: shirohoo <hch4102@naver.com>
    
    * feat(Changhun-Han)DataStructure, ArrayList 구현
    
    Signed-off-by: shirohoo <hch4102@naver.com>
    
    * feat(Changhun-Han): [BOJ, 1406] 에디터
    
    https://www.acmicpc.net/problem/1406
    Signed-off-by: shirohoo <hch4102@naver.com>
    
    * feat(Changhun-Han): [BOJ, 1926] 그림
    
    https://www.acmicpc.net/problem/1926
    Signed-off-by: shirohoo <hch4102@naver.com>
    
    * feat(Changhun-Han): [BOJ, 1926] 그림
    
    https://www.acmicpc.net/problem/1926
    Signed-off-by: shirohoo <hch4102@naver.com>
    
    * feat(Changhun-Han): [BOJ, 2217] 로프
    
    https://www.acmicpc.net/problem/2217
    Signed-off-by: shirohoo <hch4102@naver.com>
    
    * feat(Changhun-Han): [BOJ, 1343] 폴리오미노
    
    https://www.acmicpc.net/problem/1343
    Signed-off-by: shirohoo <hch4102@naver.com>
    
    * feat(Changhun-Han): [BOJ, 1758] 알바생 강호
    
    https://www.acmicpc.net/problem/1758
    Signed-off-by: shirohoo <hch4102@naver.com>
    
    * feat(Changhun-Han): [BOJ, 11399] ATM
    
    https://www.acmicpc.net/problem/11399
    Signed-off-by: shirohoo <hch4102@naver.com>
    
    * feat(Changhun-Han): [BOJ, 11399] ATM
    
    https://www.acmicpc.net/problem/11399
    Signed-off-by: shirohoo <hch4102@naver.com>
    
    * feat(Changhun-Han): [BOJ, 11508] 2+1 세일
    
    https://www.acmicpc.net/problem/11508
    Signed-off-by: shirohoo <hch4102@naver.com>
    
    * feat(Changhun-Han): [BOJ, 20115] 에너지 드링크
    
    https://www.acmicpc.net/problem/20115
    Signed-off-by: siro <hch4102@naver.com>
    
    * feat(Changhun-Han): [BOJ, 5622] 다이얼
    
    https://www.acmicpc.net/problem/5622
    Signed-off-by: siro <hch4102@naver.com>
    
    * feat(Changhun-Han): [BOJ, 2941] 크로아티아 알파벳
    
    https://www.acmicpc.net/problem/2941
    Signed-off-by: siro <hch4102@naver.com>
    
    * feat(Changhun-Han): [BOJ, 20300] 서강근육맨
    
    https://www.acmicpc.net/problem/20300
    Signed-off-by: siro <hch4102@naver.com>
    
    * feat(Changhun-Han): [BOJ, 13164] 행복유치원
    
    https://www.acmicpc.net/problem/13164
    Signed-off-by: siro <hch4102@naver.com>
    
    * feat: 문자열 조작하기
    
    * feat(Changhun-Han)DataStructure, ArrayList 구현
    
    Signed-off-by: shirohoo <hch4102@naver.com>
    
    * feat(Changhun-Han): [BOJ, 1406] 에디터
    
    https://www.acmicpc.net/problem/1406
    Signed-off-by: shirohoo <hch4102@naver.com>
    
    * feat(Changhun-Han): [BOJ, 1926] 그림
    
    https://www.acmicpc.net/problem/1926
    Signed-off-by: shirohoo <hch4102@naver.com>
    
    * feat(Changhun-Han): [BOJ, 1926] 그림
    
    https://www.acmicpc.net/problem/1926
    Signed-off-by: shirohoo <hch4102@naver.com>
    
    * feat: 문자열 조작하기
    
    * feat(Changhun-Han):[Programmers, 42576] 완주하지 못한 선수

---
+++ Changhun-Han/src/programmers/Prm_42576.java
@@ -0,0 +1,19 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class Prm_42576 {
+
+    public String solution(String[] participant, String[] completion) {
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+
+        for (int i = 0; i < participant.length - 1; i++) {
+            if (!participant[i].equals(completion[i])) {
+                return participant[i];
+            }
+        }
+        return participant[participant.length - 1];
+    }
+
+}
