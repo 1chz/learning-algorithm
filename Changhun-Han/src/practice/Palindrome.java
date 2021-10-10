ecda50cc52d5a3ae92ef6e50a1b83da40d5d2b8c
Changhun Han <71188307+shirohoo@users.noreply.github.com>
2021-10-10 16:35:28 +0900

    [2021-10-10] 28주차 마무리 (#423)
    - 총원: 6
    - 제출: 4
    - 미제출: 2
    
    ---

---
+++ Changhun-Han/src/practice/Palindrome.java
@@ -0,0 +1,28 @@
+package practice;
+
+public class Palindrome {
+
+    public boolean solution(final String sentence) {
+        return sentence.equalsIgnoreCase(
+            new StringBuilder(sentence)
+                .reverse()
+                .toString()
+        );
+    }
+
+    public boolean solution2(String sentence) {
+        sentence = sentence.toLowerCase();
+        int len = sentence.length();
+        int endIndex = len - 1;
+        for (int lt = 0; lt < len / 2; lt++) {
+            int rt = endIndex - lt;
+            char ltCharAt = sentence.charAt(lt);
+            char rtCharAt = sentence.charAt(rt);
+            if (ltCharAt != rtCharAt) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+}
